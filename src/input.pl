f7daa3b95fdb0133ef1555b1a841d49a8a8a1ca9
JaySuave <up202004823@up.pt>
2023-10-16 01:54:16 +0100

    input initial commit

---
+++ src/input.pl
@@ -0,0 +1,49 @@
+% Define the predicates for managing row and column input
+
+% Predicate for managing the row input
+manageRow(NewRow) :-
+    readRow(Code),
+    validateRow(Code, Row).
+
+% Predicate for managing the column input
+manageColumn(NewColumn) :-
+    readColumn(Column),
+    validateColumn(Column, NewColumn).
+
+% Predicate to read the row input as char
+readRow(Code) :-
+    write('  > Row    '),
+    get_char(Char),
+    char_code(Char,Code).
+
+% Predicate to read the column input
+readColumn(Column) :-
+    write('  > Column '),
+    read(Column).
+
+% Predicate to validate the row input
+validateRow(65, 1) :-
+    write('You entered character A\n').
+validateRow(66, 2) :-
+    write('You entered character B\n').
+validateRow(67, 3) :-
+    write('You entered character C\n').
+validateRow(68, 4) :-
+    write('You entered character D\n').
+validateRow(69, 5) :-
+    write('You entered character E\n').
+validateRow(Other, _) :-
+    format('You entered ~w, which is not a valid character.\n', [Other]).
+
+
+% Predicate to validate the column input
+validateColumn(1, 1).
+validateColumn(2, 2).
+validateColumn(3, 3).
+validateColumn(4, 4).
+validateColumn(5, 5).
+
+validateColumn(_, NewColumn) :-
+    write('ERROR: That column is not valid!\n\n'),
+    readColumn(Input),
+    validateColumn(Input, NewColumn).
---
+++ src/utils.pl
@@ -0,0 +1,15 @@
+getValueFromList([H|_T], 0, Value) :-
+        Value = H.
+
+getValueFromList([_H|T], Index, Value) :-
+        Index > 0,
+        Index1 is Index - 1,
+        getValueFromList(T, Index1, Value).
+
+getValueFromMatrix([H|_T], 0, Column, Value) :-
+        getValueFromList(H, Column, Value).
+
+getValueFromMatrix([_H|T], Row, Column, Value) :-
+        Row > 0,
+        Row1 is Row - 1,
+        getValueFromMatrix(T, Row1, Column, Value).
