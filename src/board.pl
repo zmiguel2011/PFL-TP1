69cd2f492b9cc63acd7a806992d4c62f946bd339
José Miguel Isidro <93340308+zmiguel2011@users.noreply.github.com>
2023-10-24 11:38:25 +0100

    updated board creation

--- src/board.pl
+++ src/board.pl
@@ -14,69 +14,52 @@ board_create(N, Board) :-
     board_create(N, 1, Board).
 
 /**
- * board_create(+N, +I, -Board)
+ * board_create(+N, +R, -Board)
  * 
- * Create board with size N; creates lines after and including I. 
+ * Create board with size N; creates rows after and including index I. 
  */
 board_create(N, I, []) :- I > N, !.
-board_create(N, I, [Line|Board]) :-
-    board_create_line(N, I, Line),
+board_create(N, I, [Row|Board]) :-
+    board_create_row(N, I, Row),
     I1 is I+1,
     board_create(N, I1, Board).
 
 
 /**
- * board_create_line(+N, +I, -Line)
+ * board_create_row(+N, +I, -Row)
  * 
- * Create board line I in a board of width N
+ * Create board row I (index) in a board of width N
  */
-board_create_line(N, I, Line) :- board_create_line(N, I, 1, Line).
+board_create_row(N, I, Row) :- board_create_row(N, I, 1, Row).
 
  /**
- * board_create_line(+N, +I, +J, -Line)
+ * board_create_row(+N, +I, +J, -Row)
  * 
- * Create board line I in a board of width N, starting in column J
+ * Create board row I in a board of width N, starting in column J
  */
-board_create_line(N, _I, J, []) :- J > N, !.
-board_create_line(N, 1, 1, [greenGoal|Board]):-     % condition for greenGoal: top left corner which is (1,1)
-    board_create_line(N, 1, 2, Board),
-    !.
-board_create_line(N, I, J, [blueGoal|Board]):-      % condition for blueGoal: bottom right corner which is (N,N)
-    I == N, J == N,
-    J1 is J+1, 
-    board_create_line(N, I, J1, Board),
-    !.
-board_create_line(N, 1, J, [inaccessible|Board]):-  % #1 condition for inaccessible: top right corner which is (1,N)
-    J == N,
-    J1 is J+1, 
-    board_create_line(N, 1, J1, Board),
-    !.
-board_create_line(N, I, 1, [inaccessible|Board]):-  % #2 condition for inaccessible: top right corner which is (N,1)
-    I == N,
-    board_create_line(N, I, 2, Board),
-    !.
-board_create_line(N, 1, J, [blue|Board]):-          % #1 condition for blue: same row as greenGoal and only occupying X cells (for any N, X is (N+1)/2 - 1)
+board_create_row(N, _I, J, []) :- J > N, !.
+board_create_row(N, 1, J, [blue|Board]):-          % #1 condition for blue: same row as greenGoal and only occupying X cells (for any N, X is (N+1)/2 - 1)
     J =< (N+1)/2,
     J1 is J+1, 
-    board_create_line(N, 1, J1, Board),
+    board_create_row(N, 1, J1, Board),
     !.
-board_create_line(N, I, 1, [blue|Board]):-          % #2 condition for blue: same column as greenGoal and only occupying X cells (for any N, X is (N+1)/2 - 1)
+board_create_row(N, I, 1, [blue|Board]):-          % #2 condition for blue: same column as greenGoal and only occupying X cells (for any N, X is (N+1)/2 - 1)
     I =< (N+1)/2,
-    board_create_line(N, I, 2, Board),
+    board_create_row(N, I, 2, Board),
     !.
-board_create_line(N, I, J, [green|Board]):-         % #1 condition for green: same row as blueGoal and only occupying X cells (for any N, X is (N+1)/2 - 1)
-    I == N, J >= (N+1)/2,
+board_create_row(N, N, J, [green|Board]):-         % #1 condition for green: same row as blueGoal and only occupying X cells (for any N, X is (N+1)/2 - 1)
+    J >= (N+1)/2,
     J1 is J+1, 
-    board_create_line(N, I, J1, Board),
+    board_create_row(N, N, J1, Board),
     !.
-board_create_line(N, I, J, [green|Board]):-         % #2 condition for green: same column as blueGoal and only occupying X cells (for any N, X is (N+1)/2 - 1)
-    J == N, I >= (N+1)/2,
-    J1 is J+1, 
-    board_create_line(N, I, J1, Board),
+board_create_row(N, I, N, [green|Board]):-         % #2 condition for green: same column as blueGoal and only occupying X cells (for any N, X is (N+1)/2 - 1)
+    I >= (N+1)/2,
+    J1 is N+1, 
+    board_create_row(N, I, J1, Board),
     !.
-board_create_line(N, I, J, [empty|Board]):-         % condition for empty: if none of the above cases, cell is empty
+board_create_row(N, I, J, [empty|Board]):-         % condition for empty: if none of the above cases, cell is empty
     J1 is J+1, 
-    board_create_line(N, I, J1, Board),
+    board_create_row(N, I, J1, Board),
     !.
 
 
@@ -95,5 +78,9 @@ initial_board(Board) :-
  * Sets up initial game state.
  */
 initial_state(Size, gamestate(Board, 1)) :-
-    board_create(Size, Board).
+    board_create(Size, Board1),                             % creates board with given Size and fills blue, green and empty cells
+    replaceInBoard(Board1, 1, 1, greenGoal, Board2),        % updates greenGoal cell 
+    replaceInBoard(Board2, Size, Size, blueGoal, Board3),   % updates blueGoal cell 
+    replaceInBoard(Board3, 1, Size, inaccessible, Board4),  % updates inaccessible cell #1
+    replaceInBoard(Board4, Size, 1, inaccessible, Board).   % updates inaccessible cell #2
     %initial_board(Board).
